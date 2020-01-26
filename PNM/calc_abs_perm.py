120cbc0018bbcb3399bfdcb032e8450a1da6db55
Zakhar Lanetc <lanetszb@gmail.com>
2020-01-26 20:06:11 +1100

    PNM: added separated files for PN extraction and Perm calculation

---
+++ PNM/calc_abs_perm.py
@@ -0,0 +1,80 @@
+import sys
+import os
+import pickle
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_path, '/Users/z5087785/data/projects/tmp/pmeal/OpenPNM/'))
+sys.path.append(os.path.join(current_path, '/Users/z5087785/data/projects/tmp/pmeal/porespy/'))
+
+import porespy as ps
+import matplotlib.pyplot as plt
+import imageio
+import scipy as sp
+import scipy.ndimage as spim
+import openpnm as op
+from porespy.filters import find_peaks, trim_saddle_points, trim_nearby_peaks
+from skimage.morphology import watershed
+from porespy.tools import randomize_colors
+
+import numpy as np
+
+
+# This demo project allows to create a pore network using PoreSPY and export it for later usage in OpenPNM
+
+# Load net file produced by PoreSpy into OpenPNM
+net = op.io.Dict.load(f'gambier_limestone.net')
+
+# Creating network dictionary which stores the required properties
+pn = op.network.GenericNetwork()
+pn.update(net)
+
+# Double-check isolating pores
+
+h = pn.check_network_health()
+op.topotools.trim(network=pn, pores=h['trim_pores'])
+
+prj = pn.project
+prj.export_data(filename='gambier_limestone', filetype='vtk')
+
+# permeability calc
+
+# Adding required fluid props and calculate hydraulic conductance
+water = op.phases.Water(network=pn)
+water.add_model(propname='throat.hydraulic_conductance',
+                model=op.models.physics.hydraulic_conductance.hagen_poiseuille)
+
+water['pore.viscosity'] = 10e-4
+water['throat.viscosity'] = 10e-4
+
+# Option to calculate hydraulic conductance manually
+# water['throat.hydraulic_conductance'] = np.pi * R ** 4 / (8 * mu_w * L)
+
+# In/out pressures
+Pout = 101325
+Pin = 2 * Pout
+
+# Specifying boundary conditions and run the solver
+flow = op.algorithms.StokesFlow(network=pn, phase=water)
+flow.set_value_BC(pores=pn.pores('left'), values=Pin)
+flow.set_value_BC(pores=pn.pores('right'), values=Pout)
+flow.run()
+
+# Calculate flow length and are
+Lx = sp.amax(pn['pore.coords'][:, 0]) - sp.amin(pn['pore.coords'][:, 0])
+A = Lx * Lx  # Since the network is cubic Lx = Ly = Lz
+
+# Add pore pressure to output file
+pn['pore.pressure'] = flow['pore.pressure']
+
+# Calc total flow rate
+Q = flow.rate(pores=pn.pores('left'))
+
+# Calc permeability and output permeability and flow rate
+K_w = flow.calc_effective_permeability(domain_area=A, domain_length=Lx)
+print("K_w = ", K_w)
+print('\n')
+print("Q=", Q)
+
+# Save PN data into CSV file
+op.io.CSV.save(pn, filename='PN_gambier_imestone')
+
---
+++ PNM/extract_pore_net.py
@@ -0,0 +1,71 @@
+import sys
+import os
+import pickle
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_path, '/Users/z5087785/data/projects/tmp/pmeal/OpenPNM/'))
+sys.path.append(os.path.join(current_path, '/Users/z5087785/data/projects/tmp/pmeal/porespy/'))
+
+import porespy as ps
+import matplotlib.pyplot as plt
+import imageio
+import scipy as sp
+import scipy.ndimage as spim
+import openpnm as op
+from porespy.filters import find_peaks, trim_saddle_points, trim_nearby_peaks
+from skimage.morphology import watershed
+from porespy.tools import randomize_colors
+
+import numpy as np
+
+
+# This demo project allows to create a pore network using PoreSPY and export it for later usage in OpenPNM
+
+# This function allows exporting and processing segmenting image from .raw file
+
+def read_raw_file(raw_file_name):
+    dim_file_name = raw_file_name[0:-4] + '.txt'
+
+    with open(dim_file_name, 'r') as dim_file:
+        line = dim_file.readline()
+        dims = list()
+        for word in line.split():
+            dims.append(int(word))
+        dims.reverse()
+
+    with open(raw_file_name, 'rb') as raw_file:
+        data = list(raw_file.read())
+
+    image = np.reshape(np.array(data, dtype=bool), (dims[0], dims[1], dims[2]))
+    image = np.transpose(image)
+
+    return image
+
+
+# This function allows to flip void/rock phases if needed
+def flip_values(data, val1, val2):
+    data = np.where(data == val1, -999.25, data)
+    data = np.where(data == val2, val1, data)
+    data = np.where(data == -999.25, val2, data)
+    return data
+
+
+# im_dat = read_dat_file('Berea.dat')
+im = read_raw_file('segmented_Gambier_512.raw')
+
+# Plots one layer of the image in 2D
+plt.imshow(im[0])
+plt.show()
+
+im = flip_values(im, 0, 1).astype(bool)
+
+# Runs pore network extraction algorithm
+net = ps.networks.snow(im, voxel_size=3.024e-6)
+
+# Exporting network for openpnm
+ps.io.to_openpnm(net, filename='gambier_limestone')
+
+# Opportunity to graphically match segmented image and produced PN (does not seem to work properly)
+
+# im = ps.tools.align_image_with_openpnm(im)
+# imageio.mimsave('extracted_network.tif', sp.array(im, dtype=np.int32))
